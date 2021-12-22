44a35e5ae73cd904afef884a0ce46bd4bedba13e
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-22 00:19:52 -0400

    Chore: initialize project with next and change directory structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>TRACTIAN | Kochiyama&apos;s redesign and implementation</title>
+			</Head>
+
+			<div>
+				<h1>Landing page</h1>
+			</div>
+		</>
+	)
+}
+
+export default Home
