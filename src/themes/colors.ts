a4334d4213b106c73204d8806eea8193c395fb35
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-22 01:08:29 -0400

    Chore: create chakra color theme

---
+++ src/themes/colors.ts
@@ -0,0 +1,23 @@
+export const colorScheme = {
+	brand: {
+		100: '#263476',
+		200: '#27388A',
+		300: '#546BBE',
+	},
+	complementar: {
+		100: '#55C25C',
+		200: '#6FDD76',
+	},
+	primary: {
+		100: '#FFFFFF',
+		200: '#F3F4F6',
+		300: '#DCDEE0',
+	},
+	secondary: {
+		100: '#121827',
+		200: '#6B7280',
+	},
+	error: {
+		100: '#EE5656',
+	},
+}
