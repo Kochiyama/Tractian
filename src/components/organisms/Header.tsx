2027aa6ff21a9e353b181428fa32b896ffc97d31
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-23 02:16:03 -0400

    Refactor: header padding dynamic

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,17 +1,61 @@
 import { Center, Container, useBreakpointValue } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { LanguageSelect } from '../atoms/LanguageSelect'
 import { Logo } from '../atoms/Logo'
 import { NavigationBar } from '../molecules/NavigationBar'
 import { Drawer } from './Drawer'
 
 export function Header() {
+	const [scrolledPercentage, setScrolledPercentage] = useState(0)
+	const [headerPadding, setHeaderPadding] = useState('2rem')
+	// from 2 to 0.5
 	const isWide = useBreakpointValue({ base: false, md: true })
 
+	const handleScroll = () => {
+		// percentage of viewport height scrolled
+		const scrolled = (window.pageYOffset * 100) / window.innerHeight
+		setScrolledPercentage(scrolled)
+		handleUpdatePadding()
+	}
+
+	useEffect(() => {
+		if (scrolledPercentage >= 100) {
+			return
+		}
+
+		const padding = 2 - 2 * (scrolledPercentage / 100)
+		if (padding <= 0.5) {
+			if (headerPadding !== '0.5rem') {
+				setHeaderPadding('0.5rem')
+			}
+
+			return
+		}
+
+		setHeaderPadding(`${padding.toFixed(2)}rem`)
+	}, [scrolledPercentage])
+
+	const handleUpdatePadding = () => {}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true })
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
+
 	return (
-		<Center bgColor='brand.200' position='fixed' w='100vw'>
+		<Center
+			bgColor='brand.200'
+			position='fixed'
+			w='100vw'
+			zIndex={99999}
+			transition='ease 300ms'
+		>
 			<Container
 				maxW='1200px'
-				p='0.5rem'
+				py={headerPadding}
 				d='flex'
 				flexDir='row'
 				justifyContent='space-between'
